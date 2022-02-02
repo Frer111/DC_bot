4bc2d0268a3f0e947d234291360a83497410bc31
Frer111 <89639231+Frer111@users.noreply.github.com>
2022-02-03 01:11:27 +0800

    hi

--- bot.py
+++ bot.py
@@ -1,2 +1,11 @@
 import discord
-hello
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event()
+async def on_ready():
+    print('>>我就爛以上線 恩~ 好啦閉嘴啦<<')
+
+bot.run('OTM4NDIwNDUzNDMxNzM0MzE2.YfqCJw.QqjjVwGpOkwtYdinZLYhcXFBM-Q')
+
