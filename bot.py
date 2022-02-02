ccfd456035374016bfab140261e5e28f8f2e1cb3
Frer111 <89639231+Frer111@users.noreply.github.com>
2022-02-03 02:06:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='>>')
 
-@bot.event()
-async def on_ready():
-    print('>>我就爛以上線 恩~ 好啦閉嘴啦<<')
+@bot.event
+async def on_connect():
+    print('>>我就爛已上線 恩~ 好啦閉嘴啦<<')
+
+async  def on_ready():
+    print('已上線')
 
 bot.run('OTM4NDIwNDUzNDMxNzM0MzE2.YfqCJw.QqjjVwGpOkwtYdinZLYhcXFBM-Q')
 
